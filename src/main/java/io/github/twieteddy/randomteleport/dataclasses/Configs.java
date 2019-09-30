66144814a7d62d9a230bc774c05e96eb41f487ae
Teddy <twieteddy@gmail.com>
2019-09-30 15:10:22 +0200

    Fix border mode value

--- src/main/java/io/github/twieteddy/randomteleport/dataclasses/Configs.java
+++ src/main/java/io/github/twieteddy/randomteleport/dataclasses/Configs.java
@@ -3,6 +3,6 @@ package io.github.twieteddy.randomteleport.dataclasses;
 public class Configs {
 
   public static final Boolean SAFE_TELEPORT = false;
-  public static final String BORDER_MODE = "vanilla";
+  public static final String BORDER_MODE = "border_mode";
   public static final int MAX_TRIES = 10;
 }
